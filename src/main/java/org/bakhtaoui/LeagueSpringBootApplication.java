008cef16c0849b06205c61f7715aeb619287de32
toufik Bakhtaoui <toufik.bakhtaoui@gmail.com>
2025-12-24 13:56:37 +0100

    [config] : add spring boot and dependencies

---
+++ src/main/java/org/bakhtaoui/LeagueSpringBootApplication.java
@@ -0,0 +1,12 @@
+package org.bakhtaoui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueSpringBootApplication {
+
+    static void main(String[] args) {
+        SpringApplication.run(LeagueSpringBootApplication.class, args);
+    }
+}
